ea0c98af1f278e46978976e8c4dfdfec3cf71ae4
unknown <RuckingFS@outlook.es>
2022-11-22 17:08:41 -0300

    casi listo

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -13,6 +13,7 @@ function QuoteBox({quoteRandom, handleClick, colorRandom}) {
   return (
     <div>
         <article className='card' style= {objStyle}>
+            <i className="card_icon fa-solid fa-quote-left"></i>
             <p className='card_quote'>  {quoteRandom.quote} </p>
             <h1 className='card_author'> {quoteRandom.author} </h1>
             <button style={objStyleBtn} className='card_btn' onClick={handleClick} >&gt;</button>
